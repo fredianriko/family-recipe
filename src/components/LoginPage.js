212792e7a436233701d2aef8e7109b1ab442cd52
Fredi Anriko <fredianriko@gmail.com>
2021-09-24 14:49:25 +0700

    styling login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function LoginPage() {
-  return <div className="login-page"></div>;
+  return (
+    <div className="login-page">
+      <form className="user-login-form">
+        <h1>Login</h1>
+        <label className="login-label">Username</label>
+        <input type="text" placeholder="username" className="login-input" />
+        <label className="login-label">Password</label>
+        <input type="password" placeholder="input your password here" className="login-input" />
+        <button className="login-button">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
